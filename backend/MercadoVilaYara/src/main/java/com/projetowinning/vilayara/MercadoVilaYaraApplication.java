15b1e87111d9acf18d2d06fbcf41a1e66f8d3080
Natália Sabino <na.ssilva@outlook.com>
2021-11-19 07:45:06 -0300

    deploy inicial da api

--- backend/MercadoVilaYara/src/main/java/com/projetowinning/vilayara/MercadoVilaYaraApplication.java
+++ backend/MercadoVilaYara/src/main/java/com/projetowinning/vilayara/MercadoVilaYaraApplication.java
@@ -2,10 +2,21 @@ package com.projetowinning.vilayara;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class MercadoVilaYaraApplication {
 
+	@GetMapping
+    public ModelAndView swaggerUi() {
+        return new ModelAndView("redirect:/swagger-ui/");
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MercadoVilaYaraApplication.class, args);
 	}
